69911197ba1e6268d2540070d1f38207ec25a9dd
mirzafeberanyy <mirzafeberani@gmail.com>
2025-09-11 15:34:38 +0700

    inisialisasi

---
+++ src/modul1_1/Main.java
@@ -0,0 +1,8 @@
+package modul1_1;
+
+public class Main {
+    // Fungsi khusus sebagai entry point dari aplikasi yang dibuat.
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
